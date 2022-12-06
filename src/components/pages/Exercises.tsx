449c1fc945aa5e5760b57dcea2990610a2419793
Sam Green <samsfgreen@gmail.com>
2022-12-06 18:42:57 +0000

    starting to add search and filter to exercises, front end only

--- src/components/pages/Exercises.tsx
+++ src/components/pages/Exercises.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
 import { trpc } from '@/utils/trpc';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CreateExerciseModal from './CreateExerciseModal';
 import Card from '../ui/Card';
 import type { ExerciseProps, ExerciseEntryProps } from '@/types/exercise';
@@ -16,7 +16,9 @@ import {
   IonMenuButton,
   IonSpinner,
   IonButton,
-  IonIcon
+  IonIcon,
+  IonSearchbar,
+  IonChip
 } from '@ionic/react';
 import { add } from 'ionicons/icons';
 import type { FunctionComponent } from 'react';
@@ -47,7 +49,28 @@ const AllExercises: NextPage<ExerciseProps> = ({ exercises }) => {
 
 const Exercises: NextPage = () => {
   const [createExerciseModalOpen, setCreateExerciseModalOpen] = useState(false);
-  const exercises = trpc.exercise.getAll.useQuery();
+  const [results, setResults] = useState([]);
+  const [searchText, setSearchText] = useState('');
+  const [filterFavourites, setFilterFavourites] = useState(false);
+  const { data: exercises } = trpc.exercise.getAll.useQuery();
+
+  useEffect(() => {
+    if (filterFavourites) {
+      setResults(
+        exercises.filter(
+          (exercise) =>
+            exercise.favourited &&
+            exercise.title.toLowerCase().indexOf(searchText) > -1
+        )
+      );
+    } else if (searchText.length > 0) {
+      setResults(
+        exercises.filter((d) => d.title.toLowerCase().indexOf(searchText) > -1)
+      );
+    } else {
+      setResults(exercises);
+    }
+  }, [filterFavourites, searchText, exercises]);
 
   const insertMutation = trpc.exercise.insertOne.useMutation({
     onSuccess: () => {
@@ -57,6 +80,18 @@ const Exercises: NextPage = () => {
       console.log(data.message);
     }
   });
+
+  const handleChange = (ev: Event) => {
+    let query = '';
+    const target = ev.target as HTMLIonSearchbarElement;
+    if (target) query = target.value!.toLowerCase();
+    setSearchText(query);
+  };
+
+  const handleFilterFavourites = () => {
+    console.log('filter favourites');
+    setFilterFavourites(!filterFavourites);
+  };
 
   return (
     <IonPage>
@@ -83,7 +118,17 @@ const Exercises: NextPage = () => {
             <IonTitle size="large">Exercises</IonTitle>
           </IonToolbar>
         </IonHeader>
-        {exercises.data && <AllExercises exercises={exercises.data} />}
+        <IonSearchbar
+          debounce={1000}
+          onIonChange={(ev) => handleChange(ev)}
+        ></IonSearchbar>
+        <IonChip
+          onClick={handleFilterFavourites}
+          color={filterFavourites ? 'primary' : 'dark'}
+        >
+          Favourites
+        </IonChip>
+        {results && <AllExercises exercises={results} />}
       </IonContent>
       <CreateExerciseModal
         isOpen={createExerciseModalOpen}
